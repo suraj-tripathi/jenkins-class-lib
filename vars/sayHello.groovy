53f4bf79a6e0961c6853a69130a59018b125b5bb
suraj-tripathi <surajtripathi167@gmail.com>
2025-10-05 16:15:36 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,4 +1,4 @@
-def call(String text) {
-  echo "Running this in vars by ${text}"
+def call(String Name) {
+  echo "Running this in vars by ${Name}"
   echo "Current build number is: ${this.env.BUILD_NUMBER}"
 }
